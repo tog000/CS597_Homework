e7ec76b77d5925cfaafb32b02350d3b7976b5e77
Gabriel Trisca <tog000@gmail.com>
2013-08-29 10:07:38 -0600

    initial import

---
+++ assign-1/src/main/java/edu/boisestate/cs597/App.java
@@ -0,0 +1,22 @@
+package edu.boisestate.cs597;
+
+/**
+ * DO NOT DELETE this class
+ * The automatic assignment checker will instantiate this class from your jar
+ * You may modify the contents of the main method to instantiate and invoke
+ * methods on the additional classes that you have created for this assignment.
+ *
+ */
+public class App 
+{
+
+    public static void main( String[] args )
+    {
+	//Replace the contents of this method with contents of your code
+        System.out.println( "Hello World!\n Arguments:" );
+	for(String arg: args){
+		System.out.println(arg);
+	}
+	System.out. println("exiting ...");
+    }
+}
