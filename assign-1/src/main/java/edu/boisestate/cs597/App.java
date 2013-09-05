41ae534a995622e6b779a81980e053de3a93d748
Gabriel Trisca <tog000@gmail.com>
2013-09-04 18:56:13 -0600

    Reading wrong indices from the input, fixed now (Breaks query #3)

--- assign-1/src/main/java/edu/boisestate/cs597/App.java
+++ assign-1/src/main/java/edu/boisestate/cs597/App.java
@@ -191,7 +191,7 @@ public class App {
 					
 					// Some lines are incomplete, filter
 					if(parts.length > 22){
-						visitee = new PersonVisits((parts[21]+" "+parts[22]).toUpperCase());
+						visitee = new PersonVisits((parts[19]+" "+parts[20]).toUpperCase());
 					}else{
 						visitee = new PersonVisits("NO NAME");
 					}
@@ -385,9 +385,9 @@ public class App {
 
 	public static void main(String[] args) throws Exception {
 		
-		/**
+		/**/
 			//new App("WhiteHouse-WAVES-2012.csv", "./", 1);
-			//new App("WhiteHouse-WAVES-2012.csv", "./", 2);
+			new App("WhiteHouse-WAVES-2012.csv", "./", 2);
 			//new App("WhiteHouse-WAVES-2012.csv", "./", 3);
 		/*/
 		if(args.length < 3){
